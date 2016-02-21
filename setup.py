368b0a8e9bf489ec3fa4fb252fc57d2d0d5683c0
merry-bits <merry-bits@users.noreply.github.com>
2016-02-21 20:58:12 +0100

    Fix for Python 2 and excluded Python 3.2

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ setup(
         "Natural Language :: English",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     keywords=(
         "flacon riemann monitoring measure request time end-point duration"),
